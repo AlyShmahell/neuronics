ba7bef70ee55633c3f60eca77189903bb6a857cf
AlyShmahell <aly.shmahell@gmail.com>
2019-08-07 05:04:56 +0200

    added first prototype for mutual_information_grad

--- neuronics/opensource/lib/py/tf/shannon/__init__.py
+++ neuronics/opensource/lib/py/tf/shannon/__init__.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import os.path
 import tensorflow as tf
+from tensorflow.python.framework import ops
 
 entropy_op = tf.load_op_library(
     os.path.join(tf.resource_loader.get_data_files_path(),
